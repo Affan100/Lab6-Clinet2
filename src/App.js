8d07eed10d2cd415e309691b8063a24ace90ea85
Affan100 <affan_patan@hotmail.com>
2020-03-03 19:38:33 +0700

    Edit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Baer from './components/Bear'
+import Student from './components/Student'
 
 const App = () => {
   return (
     <div className="Mov">
       <h1>Affan Pathan 6035512016 Lab06</h1>
-      <Baer />
+      <Student />
     </div>
   );
 }
--- src/components/Student.js
+++ src/components/Student.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import './bear.css'
+import './Student.css'
 
 export default () => {
 
